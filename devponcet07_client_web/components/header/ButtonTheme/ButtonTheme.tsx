5c76513142c341e288e917daf16e46c68a884026
Adrien <devponcet07@gmail.com>
2026-05-18 16:18:07 +0200

    fix theme

--- devponcet07_client_web/components/header/ButtonTheme/ButtonTheme.tsx
+++ devponcet07_client_web/components/header/ButtonTheme/ButtonTheme.tsx
@@ -2,23 +2,56 @@
 import { useEffect, useState } from "react"
 import styles from "./ButtonTheme.module.css"
 
+
+type Theme = "light" | "dark";
+
+function getSystemTheme(): Theme {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+}
+
+
 export default function ButtonTheme() {
-  const [isDark, setIsDark] = useState(false)
-  console.log("test")
+    const [mounted, setMounted] = useState(false);
+    const [theme, setTheme] = useState<Theme>("light");
 
-  useEffect(() => {
-    console.log(isDark)
-    document.documentElement.setAttribute("data-theme", isDark ? "dark" : "light")
-  }, [isDark])
 
-  return (
-    <label className={styles.switch}>
-      <input
-        type="checkbox"
-        checked={isDark}
-        onChange={() => setIsDark((prev) => !prev)}
-      />
-      <span className={styles.slider}></span>
-    </label>
-  )
+    useEffect(() => {
+        const saved = localStorage.getItem("theme") as Theme | null;
+
+        const initialTheme = saved ?? getSystemTheme();
+
+        setTheme(initialTheme);
+        document.documentElement.setAttribute("data-theme", initialTheme);
+        setMounted(true);
+    }, []);
+
+
+    useEffect(() => {
+        if (!mounted) return;
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme,mounted]);
+
+    const toggle = () => {
+        setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+    };
+
+     if (!mounted) {
+    // 🔥 important: évite flash + mismatch
+    return null;
+  }
+
+
+    return (
+        <label className={styles.switch}>
+            <input
+                type="checkbox"
+                checked={theme === 'dark' ? true : false}
+                onChange={toggle}
+            />
+            <span className={styles.slider}></span>
+        </label>
+    )
 }
