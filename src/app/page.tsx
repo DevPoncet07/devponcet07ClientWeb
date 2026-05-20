66d5570f233c58a093e79085728d639db94cc31a
Adrien <devponcet07@gmail.com>
2026-05-20 16:36:50 +0200

    add hero section and carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import App from "@/components/app/App";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import Main from "@/components/main/Main";
+import Hero from "@/components/sections/hero/Hero"
 
 
 export default function Home() {
   return (<App>
     <Header/>
     <Main>
-      <p>home</p>
+      <Hero/>
     </Main>
     <Footer/>
     </App>
---
+++ src/components/sections/hero/Hero.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Image from "next/image";
+import styles from "./Hero.module.css";
+
+const slides = [
+  "/static/heroCarouselImage/carousel1.png",
+  "/static/heroCarouselImage/carousel2.png",
+  "/static/heroCarouselImage/carousel3.png",
+  "/static/heroCarouselImage/carousel4.png",
+  "/static/heroCarouselImage/carousel1.png",
+  "/static/heroCarouselImage/carousel2.png",
+  "/static/heroCarouselImage/carousel3.png",
+  "/static/heroCarouselImage/carousel4.png",
+];
+
+export default function Hero() {
+
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroCarousel}>
+        {slides.map((slide) => 
+          <div className={styles.heroCarouselSlide}>
+            <Image
+              src={slide}
+              width={500}
+              height={500}
+              alt=""
+              loading="eager"
+              className={styles.heroCarouselImage}
+            />
+          </div>
+        )}
+        </div>
+    </section>
+  );
+}
